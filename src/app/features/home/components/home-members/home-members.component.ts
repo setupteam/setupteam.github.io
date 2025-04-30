9ef6396490cb7ff644486bca25bbcc0cf772dd22
AFelipeCR <andres.chaparro06@uptc.edu.co>
2025-04-30 18:55:09 +0000

    Componente miembros
    Unicamente el mockup

---
+++ src/app/features/home/components/home-members/home-members.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stdev-home-members',
+  imports: [],
+  templateUrl: './home-members.component.html',
+  styleUrl: './home-members.component.scss'
+})
+export class HomeMembersComponent {
+
+}
