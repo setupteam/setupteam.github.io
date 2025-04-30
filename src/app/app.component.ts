195dc646f7bc10d666b899aaadf287680919ec99
AFelipeCR <andres.chaparro06@uptc.edu.co>
2025-04-30 00:55:23 +0000

    personalizacion de código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'stdev-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
